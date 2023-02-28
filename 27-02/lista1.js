4960f0694c774f565d9cbfaf8c9ce95b0d6ecb6b
Daniel Pires <dfpires@gmail.com>
2023-02-28 09:17:35 -0300

    Delete 27-02 directory

--- 27-02/lista1.js
+++
@@ -1,9 +0,0 @@
-function calcularExe1(){
-    // recupera dados do formulário
-    let nro1 = document.getElementById("nro1").value
-    let nro2 = document.getElementById("nro2").value
-    // processamento
-    let subtracao = nro1 - nro2
-    // saída dos dados
-    document.getElementById("subtracao").innerHTML = "O resultado é " + subtracao
-}
