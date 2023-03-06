6484062393ae585e6f8ff7967019204f94685a76
Daniel Pires <dfpires@gmail.com>
2023-03-06 08:37:56 -0300

    Add files via upload

--- 14-02/boxmodel.js
+++ 14-02/boxmodel.js
@@ -8,4 +8,20 @@ function calcular(){
     let imc
     imc = peso / (altura * altura)
     alert("O seu imc é " + imc.toFixed(2))    
+
+    if (imc < 18.5){
+        alert('Magreza')
+    }
+    else if ((imc >= 18.5) && (imc <= 24.9)){
+        alert('Normal')
+    }
+    else if ((imc >= 25) && (imc <= 29.9)){
+        alert('Sobrepeso')
+    }
+    else if (imc <= 39.9){
+        alert('Obesidade 1')
+    }
+    else {
+        alert('Obesidade 2')
+    }
 }
