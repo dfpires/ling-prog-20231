d2d19ea3d8bf84f0f2e6759630071616e7da7acb
Daniel Pires <dfpires@gmail.com>
2023-04-24 09:13:36 -0300

    Add files via upload

--- lista5-vet-objetos/lista.js
+++ lista5-vet-objetos/lista.js
@@ -35,3 +35,78 @@ function exe2(){
     console.log(`Maior salário ${maiorSalario}`)
     console.log(`% de mulheres salário > 1000 ${contaFS/contaF*100}`)
 }
+function exe3(){
+    let vetor = []
+    for(let i=0;i<3;i++){
+        let obj = {
+            sexo: prompt("Informe sexo: M ou F").toUpperCase(),
+            altura: Number(prompt("Informe altura")),
+            idade: Number(prompt("Informe idade")),
+            olhos: prompt("Informe cor dos olhos: A, V ou C").toUpperCase()
+        }
+        vetor.push(obj)
+    }
+    let somaIdade = 0; let contaIdade = 0
+    let maiorIdade = vetor[0].idade; let qtde = 0; let qtdeM = 0
+    for(let i=0;i<3;i++){
+        if ((vetor[i].olhos == 'C') && (vetor[i].altura > 1.60)){
+            somaIdade += vetor[i].idade
+            contaIdade++
+        }
+        if (vetor[i].idade > maiorIdade){
+            maiorIdade = vetor[i].idade
+        }
+        if (((vetor[i].sexo == 'F') && (vetor[i].idade >= 20) && (vetor[i].idade <= 45)) 
+               || ((vetor[i].olhos = 'V') && vetor[i].altura < 1.70)) {
+                qtde++
+        }
+        if (vetor[i].sexo == 'M'){
+            qtdeM++
+        }
+    }
+    alert(`A média de idades é ${somaIdade/contaIdade}`)
+    alert(`A maior idade é ${maiorIdade}`); alert(`A qtde é ${qtde}`)
+    alert(`% de homens ${qtdeM/vetor.length*100}`)
+}
+function exe6(){
+    let opcao
+    let clientes = []; let contas = [];
+    do{
+        opcao = Number(prompt(`Digite 1. Criar cliente \n 2. Criar conta \n 3. Listar contas \n 4. Sair`))
+        switch(opcao){
+            case 1: let objCliente = {
+                        numero: clientes.length, // código do cliente é incremental
+                        nome: prompt('Nome do cliente'),
+                        telefone: prompt('Telefone do cliente'),
+                        endereco: prompt('Endereço do cliente')
+                    }
+                    clientes.push(objCliente)
+                    break
+            case 2: let objConta = {
+                        numero: contas.length,
+                        nroCliente: Number(prompt('Informe número do cliente')),
+                        saldo: Number(prompt('Saldo da conta'))
+                    }
+                    // verifica se o cliente existe
+                    let achou = false
+                    for(let i=0;i<clientes.length;i++){
+                        if (objConta.nroCliente == clientes[i].numero){
+                            contas.push(objConta)
+                            achou = true // encontrou
+                        }
+                    }
+                    if (!achou){ // não encontrou o cliente
+                        let objCliente = {
+                            numero: clientes.length, // código do cliente é incremental
+                            nome: prompt('Nome do cliente'),
+                            telefone: prompt('Telefone do cliente'),
+                            endereco: prompt('Endereço do cliente')
+                        }
+                        clientes.push(objCliente)
+                        contas.push(objConta)
+                    }
+                    break
+            }
+    }
+    while (opcao != 4)
+}
